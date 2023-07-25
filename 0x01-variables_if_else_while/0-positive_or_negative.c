b4f7cb7d9bfd4dc4df12b2149aa7781e51ed7fe5
yasmin12mohamed <yasmeenm439@gmail.com.com>
2023-07-25 15:46:24 -0700

    Build C program with elseif

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,25 +1,29 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*
- * main -  main block
+
+/**
+ *main - main block
  *
- * Description:determine if number negative or positive
+ *Description:it is positive, negative, or zero.
  *
  * Return: 0
- */
+*/
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n>0){
-		printf(n, "is positive");
-	}elseif(n<0){
-		printf(n, "is negative");
-	}else(n=0){
-		printf(n, "is zero");
+
+	if (n > 0){
+		printf("%d is positive\n", n);
+	}
+	else if (n < 0){
+		printf("%d is negative\n", n);
+	}
+	else{
+		printf("%d is zero\n", n);
 	}
 	return (0);
 }
