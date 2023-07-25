4e4de90c24c3459e6eaa1423d2181680d72dc04f
yasmin12mohamed <yasmeenm439@gmail.com.com>
2023-07-25 16:25:23 -0700

    Build C program on a number

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - main block
+ *
+ * Description: program determine if the last degit of number is greater than 5 or equal to 0 or less than 6 and not 0
+ *
+ * Return: 0*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n%10 > 5)
+		printf("Last digit of %d is %d and is greater than 5");
+	elseif (n%10 <6 && !=0)
+		printf("Last digit of %d is %d and is less than 6 and not 0");
+	else (n%10 = 0)
+		printf("Last digit of %d is %d and is 0");
+	return (0);
+}
