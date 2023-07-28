5a00c436f348f5fef7f8eba5361d2b37de984b01
yasmin12mohamed <yasmeenm439@gmail.com.com>
2023-07-28 13:57:21 -0700

    Bulid a  C program to test numbers

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,8 @@ int main(void)
 	}
 	else if (last_digit < 6 && last_digit != 0)
 	{
-		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last_digit);
+	 	printf("Last digit of %i is %i and is less than 6 and not 0\n",
+			       	n, last_digit);
 	}
 	else
 	{
