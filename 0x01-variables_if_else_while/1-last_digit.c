eafd1eefacb37d63d538e62b75f9c57cfdcf7402
yasmin12mohamed <yasmeenm439@gmail.com.com>
2023-07-28 10:14:48 -0700

    Build a C program to test numbers

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,15 +18,15 @@ int main(void)
 	last_digit = n%10;
 	if(last_digit > 5)
 	{
-		printf("Last digit of %i is %i and is greater than 5\n");
+		printf("Last digit of %i is %i and is greater than 5\n", n, last_digit);
 	}
 	elseif (last_digit < 6 && !=0)
 	{
-		printf("Last digit of %i is %i and is less than 6 and not 0\n");
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n ,last_digit);
 	}
 	else
 	{
-		printf("Last digit of %i is %i and is 0\n");
+		printf("Last digit of %i is %i and is 0\n", n, last_digit);
 	}
 	return (0);
 }
