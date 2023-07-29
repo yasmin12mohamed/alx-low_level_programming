5daef994079fbeaabc2d9e5252577baf4b7570c2
yasmin12mohamed <yasmeenm439@gmail.com.com>
2023-07-28 17:45:37 -0700

    Print numbers and letters

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - main block
+ *
+ * Description: Print numbers from 0 to 15 by putchar'
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+	char c;
+
+	for (num = 0 ; num < 10 ; num ++)
+	{
+		putchar(num + '0');
+	}
+	for (c = 'a' ; c <= f ; c ++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
+
