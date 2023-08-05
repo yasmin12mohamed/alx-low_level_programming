aa34ac847796999025c0f5bd3a438dbb65fd2526
yasmin12mohamed <yasmeenm439@gmail.com.com>
2023-08-05 06:18:36 -0700

    Print Putchar fun

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stduio.h>
+/**
+ * main - main block
+ *
+ * Description: Write a program that prints _putchar
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	putchar('\n');
+	return (0);
+}
