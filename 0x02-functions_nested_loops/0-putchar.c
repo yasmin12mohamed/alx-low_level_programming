223799333ab7d26382a75be8dfa933be1bf4886f
yasmin12mohamed <yasmeenm439@gmail.com.com>
2023-08-05 06:40:14 -0700

    Print "_Putchar"

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,6 +9,13 @@
  */
 int main(void)
 {
-	putchar('\n');
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
 	return (0);
 }
