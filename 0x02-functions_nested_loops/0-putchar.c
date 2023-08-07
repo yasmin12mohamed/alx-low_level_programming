167b63230da232fb37b30847ce15b788eb76c2aa
yasmin12mohamed <yasmeenm439@gmail.com.com>
2023-08-07 15:52:40 -0700

    Print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - main block
+ *
+ * Description:print '_putchar'
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char S[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i ++)
+	{
+		_putchar(S[8]);
+		_putchar('\n');
+	}
+	return (0);
+}
