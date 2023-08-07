7357615a97b9e9a37af2376024f2629f9a1d6969
yasmin12mohamed <yasmeenm439@gmail.com.com>
2023-08-07 13:19:39 -0700

    Print _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - main block
+ *
+ * Description: prints the alphabet, in lowercase
+ *
+ * Return:0
+ */
+void print_alphabet(void)
+{
+	int c;
+
+	fot (c = 'a'; c <= z; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+	return (0);
+}
