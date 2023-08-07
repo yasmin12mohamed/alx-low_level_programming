2da3d0897e4d3f89665144d4ac99a753322c3bc0
yasmin12mohamed <yasmeenm439@gmail.com.com>
2023-08-07 14:46:21 -0700

    Test positive_or_negative() function.

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - main block
+ *
+ * description:Test positive_or_negative() function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
