91c059e68b61ae35d3955748bd1b1335c8a50c9e
yasmin12mohamed <yasmeenm439@gmail.com.com>
2023-08-09 04:45:07 -0700

    Print _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,7 +7,7 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-	int _putchar(char c)
+	int _putchar(char);
 	void print_alphabet(void);
 	void print_alphabet_x10(void);
 	int _islower(int c);
