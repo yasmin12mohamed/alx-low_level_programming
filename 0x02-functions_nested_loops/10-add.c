c61117b23e2bd472ac99859c952bfb86727cba71
yasmin12mohamed <yasmeenm439@gmail.com.com>
2023-08-18 05:36:10 -0700

    Add numbers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,12 +3,15 @@
  * add - add block
  *
  * Description:function that adds two integers and returns the result
+ * @x:parameter to be verified
+ * @y:parameter to be verified
  *
  * Return: the sum of numbers
  */
 int add(int x, int y)
 {
 	int res;
+
 	res = (x + y);
 	return (res);
 }
