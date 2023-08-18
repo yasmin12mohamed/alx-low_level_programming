59cbeca0f543a5a81986654bfbc049d34e634197
yasmin12mohamed <yasmeenm439@gmail.com.com>
2023-08-18 09:07:03 -0700

    Test a function

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,17 @@
 #include "main.h"
+#include "main.h"
+
 /**
- * main - main block
- *
- * Description:test a function
- *
- * Return: 0
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
 int main(void)
 {
-	int num;
+        int i;
 
-	num = 0;
-	positive_or_negative(num);
+        i = 0;
+        positive_or_negative(i);
 
-	return (0);
+        return (0);
 }
