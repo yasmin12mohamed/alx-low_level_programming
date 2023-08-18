92e754442ac710a6202039ec9ac33bda8e1dba5a
yasmin12mohamed <yasmeenm439@gmail.com.com>
2023-08-18 05:31:30 -0700

    Add numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include <main.h>
+/**
+ * add - add block
+ *
+ * Description:function that adds two integers and returns the result
+ *
+ * Return: the sum of numbers
+ */
+int add(int x, int y)
+{
+	int res;
+	res = (x + y);
+	return (res);
+}
