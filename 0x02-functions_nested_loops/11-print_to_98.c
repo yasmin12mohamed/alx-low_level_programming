360046b0d0cea10da0f0658fc51f552df1c6e44b
yasmin12mohamed <yasmeenm439@gmail.com.com>
2023-08-18 08:27:40 -0700

    Print from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,23 +10,22 @@
  */
 void print_to_98(int n)
 {
-	if (n > 98)
+	if (n < 98)
 	{
-		for (n = n; n >= 98; n--)
+		for (n = n; n < 98; n++)
 		{
-			putchar(n + '0');
-			putchar(',');
-			putchar(' ');
+			printf("%d, ", n);
 		}
+		printf("%d\n"98);
 	}
 	else
 	{
-		for (n = n; n <= 98; n++)
+		for(n = n; n > 98; n--)
 		{
-			putchar(n + '0');
-			putchar(',');
-			putchar(' ');
+			printf("%d, ", n);
 		}
+		printf("%d\n", 98);
 	}
-	_putchar('\n');
 }
+
+
