42fcfdebf696142be70dca7f1103aaa4d3878481
yasmin12mohamed <yasmeenm439@gmail.com.com>
2023-08-19 06:15:19 -0700

    Solve task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -9,12 +9,12 @@
  */
 int _isupper(int c)
 {
-	if (c = 'a' && c <= 'z')
-	{
-		return (0);
-	}
-	else (c = 'A' && c <= 'Z')
+	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
+	}
+	else
+	{
+		return (0);
 	}
 }
