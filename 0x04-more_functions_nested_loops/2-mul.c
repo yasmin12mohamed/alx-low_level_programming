6ae33362f26c91bc5b3de566181011cc64346ada
yasmin12mohamed <yasmeenm439@gmail.com.com>
2023-08-19 06:54:47 -0700

    Solve task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * mul - mul block
+ *
+ * Description:function that multiplies two integers.
+ * @a:parameter to be verified
+ * @b:parameter to be verified
+ *
+ * Return:the result of mutiplication
+ */
+int mul(int a, int b)
+{
+	int multi;
+
+	multi = a * b;
+	_putchar(multi);
+	return (multi);
+}
