7ebbbd3b1d8d049c665985d682524ec2862c6b54
yasmin12mohamed <yasmeenm439@gmail.com.com>
2023-08-21 07:36:41 -0700

    Solve task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_square - print_square block
+ *
+ * Description:prints a square, followed by a new line
+ * @size: parameter to be verified
+ *
+ * Return: nothing
+ */
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int column, row;
+
+		for (row = 1; row <= size; size++)
+		{
+			for (column = 1; column <= size; size++)
+			{
+				if (size == column)
+				{
+					_putchar(35);
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
