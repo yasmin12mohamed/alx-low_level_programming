a389e34dd7b760f428cf6df30c612c0be44eca74
yasmin12mohamed <yasmeenm439@gmail.com.com>
2023-08-22 14:31:26 -0700

    Solve task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _puts - _puts block
+ *
+ * Description:prints a string, followed by a new line
+ * @str:parameter to be verified
+ *
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	int x;
+
+	x = 0;
+
+	while (str[x] != '\0')
+	{
+		x++;
+	}
+	_putchar(str[x]);
+	_putchar('\n');
+}
