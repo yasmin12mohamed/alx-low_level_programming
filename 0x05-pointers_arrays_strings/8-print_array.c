400437227d7ef3dcd69fc1a0d2d5692084ba3fbd
yasmin12mohamed <yasmeenm439@gmail.com.com>
2023-08-22 15:24:17 -0700

    Solve task 6

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_array - print_array block
+ *
+ * Description:n elements of an array of integers
+ * @a:parameter to be verified
+ * @n:parameter to be verified
+ *
+ * Return: nothing
+ */
+void print_array(int *a, int n)
+{
+	int x;
+
+	for (x = 0; x < n; x++)
+	{
+		if (x == 0)
+		{
+			printf("%d", a[x]);
+		}
+		else
+		{
+			printf(", %d", a[x]);
+		}
+	}
+		printf("\n");
+}
