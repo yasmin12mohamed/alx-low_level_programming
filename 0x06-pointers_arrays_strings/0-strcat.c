7081b4d65161d706a7daa4b47b249dcedf3f72bb
yasmin12mohamed <yasmeenm439@gmail.com.com>
2023-08-22 16:09:43 -0700

    Solve task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * *_strcat - *_strcat block
+ *
+ * Description:concatenates two strings.
+ * @dest:parameter to be verified
+ * @src:parameter to be verified
+ *
+ * Return: pointer to the result string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int x, y;
+
+	x = 0;
+	y = 0;
+
+	while (dest[x] != '\0')
+	{
+		x++;
+	}
+	while (src[y] != '\0')
+	{
+		dest[x] = src[j];
+		y++;
+		x++;
+	}
+	dest[x] = '\0';
+	return (dest);
+}
