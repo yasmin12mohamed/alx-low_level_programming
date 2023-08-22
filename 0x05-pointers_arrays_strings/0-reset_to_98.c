859997a6111e5d316aec9e74f0a297de6a0f8e71
yasmin12mohamed <yasmeenm439@gmail.com.com>
2023-08-22 14:13:01 -0700

    Solve task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
  * reset_to_98 - reset_to_98 block
  *
  * Description:takes pointer to int as parameter,updates its value to 98
- * @*n: parameter to be verified
+ * @n: parameter to be verified
  *
  * Return: nothing
  */
