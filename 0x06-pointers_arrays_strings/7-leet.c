9545124badf6efd6ccb8d88f67748d41fa108e73
yasmin12mohamed <yasmeenm439@gmail.com.com>
2023-08-22 18:16:23 -0700

    Solve task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * *leet - *leet block
+ *
+ * Description:unction that encodes a string into 1337
+ * @s: string to be encoded
+ *
+ * Return: the result
+ */
+har *leet(char *s)
+{
+	int x, y;
+
+	char *w = "aAeEoOtTlL";
+	char *q = "4433007711";
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			if (s[x] == a[y])
+			{
+				s[x] = b[y];
+			}
+		}
+	}
+
+	return (s);
+}
