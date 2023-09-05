25add481d4032d12d04593f3a9b3e41ae1798843
yasmin12mohamed <yasmeenm439@gmail.com.com>
2023-09-05 16:56:23 -0700

    Task 0

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,20 +1,13 @@
-#include "main.h"
-/**
- * main - main block
- *
- * Description:print '_putchar'
- *
- * Return: 0
- */
-int main(void)
-{
-	char S[9] = "_putchar";
-	int i;
+#include <unistd.h>
 
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(S[i]);
-	}
-	_putchar('\n');
-	return (0);
+/**
+* _putchar - writes the character c to stdout
+* @c: The character to print
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
