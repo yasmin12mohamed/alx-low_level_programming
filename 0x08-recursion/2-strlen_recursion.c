2fee74ece3e5aa278a7c9e4c37943be0aa5a3579
yasmin12mohamed <yasmeenm439@gmail.com.com>
2023-09-05 11:35:35 -0700

    Solve task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen_recursion - _strlen_recursion block
+ *
+ * Description:returns the length of a string
+ * @s: string to determine the length of
+ *
+ * Return: string length
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == 0)
+		return (0);
+	return (s + _strlen_recursion(s + 1));
+}
