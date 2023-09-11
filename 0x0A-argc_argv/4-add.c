3d739445475f84b66130cb9c5015199e2ff0126b
yasmin12mohamed <yasmeenm439@gmail.com.com>
2023-09-11 16:01:48 -0700

    Task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - main block
+ *
+ * Description: prints the number of arguments passed into it.
+ * @argc: number of counts
+ * @argv: the arguments
+ *
+ * Return: (1) or (0)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 2)
+	{
+		int x;
+
+		for (x = 0; x < argc; x++)
+		{
+			if (isdigit(x))
+			{
+				int sum = 0;
+				sum = sum + atoi(argv[x]);
+				printf("%d\n", sum);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+	else
+	{
+		printf("0\n");
+	}
+	return (0);
+}
