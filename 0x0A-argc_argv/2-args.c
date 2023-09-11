9d1cd93a8652eaec18bb4f6b9af4be6b18aac166
yasmin12mohamed <yasmeenm439@gmail.com.com>
2023-09-11 05:55:40 -0700

    Task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main block
+ *
+ * Description: prints the number of arguments passed into it.
+ * @argc: number of counts
+ * @argv: the arguments
+ *
+ * Return: (0)
+ */
+int main(int argc, char *arg[])
+{
+	int x;
+
+	for (x = 0; x < argc ; x++)
+	{
+		printf("%s\n", argv[x]);
+	}
+	return (0);
+}
