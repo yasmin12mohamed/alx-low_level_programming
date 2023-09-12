6093ade738b1bf3852906ca7b25a7d22ad1103f5
yasmin12mohamed <yasmeenm439@gmail.com.com>
2023-09-12 15:39:23 -0700

    Task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,17 +12,22 @@
  */
 int main(int argc, char *argv[])
 {
-	if (argc > 2)
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else if (argc > 1)
 	{
 		int x;
 
-		for (x = 0; x < argc; x++)
+		for (x = 0; x > argc; x++)
 		{
 			if (isdigit(argv[x]))
 			{
-				int sum = 0;
+				int sum;
 
 				sum = sum + atoi(argv[x]);
+
 				printf("%d\n", sum);
 			}
 			else
@@ -31,10 +36,6 @@ int main(int argc, char *argv[])
 				return (1);
 			}
 		}
-	}
-	else if (argc == 1)
-	{
-		printf("0\n");
 	}
 	return (0);
 }
