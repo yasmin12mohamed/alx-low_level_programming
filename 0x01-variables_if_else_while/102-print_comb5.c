a23804fb7894c6bb089daa6201eabd71e2da9e95
yasmin12mohamed <yasmeenm439@gmail.com.com>
2023-09-26 22:19:19 +0300

    Advanced task 3

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -21,9 +21,13 @@ int main()
 			putchar(' ');
 			putchar(j / 10 + '0');
 			putchar(j % 10 + '0');
-			putchar(',');
-			putchar(' ');
+
+			if (i == 98 || j == 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
-	return 0;
+	return (0);
 }
