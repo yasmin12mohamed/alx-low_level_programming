b93b1168669727ee41eba342961935337e12375a
yasmin12mohamed <yasmeenm439@gmail.com.com>
2023-09-26 22:39:55 +0300

    Advanced task 3

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,28 +6,28 @@
  *
  * Return: 0
  */
-#include <stdio.h>
-
-int main() 
+int main(void)
 {
-	int i, j;
+	int num1, num2;
 
-	for (i = 0; i <= 99; i++)
+	for (num1 = 0; num1 <= 98; num1++)
 	{
-		for (j = i; j <= 99; j++)
+		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
-			putchar(i / 10 + '0');
-			putchar(i % 10 + '0');
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
 			putchar(' ');
-			putchar(j / 10 + '0');
-			putchar(j % 10 + '0');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
 
-			if (i == 98 || j == 99)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+			if (num1 == 98 && num2 == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
 		}
 	}
+	putchar('\n');
+
 	return (0);
 }
