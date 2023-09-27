6eb611ce7fd9290157a406dfb48bb97ff56bb34f
yasmin12mohamed <yasmeenm439@gmail.com.com>
2023-09-27 01:23:12 -0700

    Advanced task 1

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_times_table - print_times_table block
+ *
+ * Description: print time tables from 0 to 15
+ * @n: number to print time table of
+ *
+ * Return: nothing
+ */
+void print_times_table(int n)
+{
+	int i, j, result
+	n = 15;
+
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				result = i * j;
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+}	
