52ce209e826bbc26bd9189dfb0e712a4543a6cbd
yasmin12mohamed <yasmeenm439@gmail.com.com>
2023-09-27 08:56:19 -0700

    Advanced task 2

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,13 +2,14 @@
 /**
  * main - main block
  *
- * Description:computes and prints sum of all the multiples of 3 or 5 below 1024
+ * Description: prints sum of all the multiples of 3 or 5 below 1024
  *
  * Return: 0
  */
 int main(void)
 {
 	int x, sum;
+
 	sum = 0;
 
 	for (x = 0; x < 1024; x++)
@@ -16,9 +17,8 @@ int main(void)
 		if ((x % 3 == 0) && (x % 5 == 0))
 		{
 			sum = sum + x;
-			printf("%d", sum);
 		}
+		printf("%d\n", sum);
 	}
-	putchar('\n');
 	return (0);
 }
