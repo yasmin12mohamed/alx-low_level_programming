86d357cb7fcfd78beb95ff5a9ac6f4843715b21c
yasmin12mohamed <yasmeenm439@gmail.com.com>
2023-09-27 08:03:42 -0700

    Advanced task 1

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,8 +10,9 @@
 void print_times_table(int n)
 {
 	int i, j, result;
-	n = 15;
-
+	
+	if (n > 0 && n <= 15)
+	{
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
@@ -22,4 +23,5 @@ void print_times_table(int n)
 				_putchar(' ');
 			}
 		}
-}	
+	}
+}
