e84701760727e8d7eab0c99b532e35e7e126bf32
yasmin12mohamed <yasmeenm439@gmail.com.com>
2023-09-28 16:13:11 -0700

    Task 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -15,10 +15,13 @@ int factorial(int n)
 	}
 	else if (n == 0)
 	{
-		return (0);
+		return (1);
 	}
 	else
 	{
-		return (n * factorial(n - 1));
+		int result;
+
+		result = n * factorial(n - 1);
+		return(
 	}
 }	
