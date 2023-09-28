9e2b75240900768f0373418e390aa1a738a52f78
yasmin12mohamed <yasmeenm439@gmail.com.com>
2023-09-28 15:37:45 -0700

    Task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -11,10 +11,10 @@ int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		return(0);
+		return (0);
 	}
 	else
 	{
-		return (_strlen_recursion(s + 1));
+		return (1 + _strlen_recursion(s + 1));
 	}
 }
