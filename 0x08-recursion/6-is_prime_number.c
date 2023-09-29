25dfbc334ecf0ff927f4b85343c73ada21f3cd7c
yasmin12mohamed <yasmeenm439@gmail.com.com>
2023-09-28 17:23:05 -0700

    Task 5

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -17,7 +17,7 @@ int _prime(int n, int i)
 	{
 		return (0);
 	}
-	return (_prime(n, i - 1));
+	return (_prime(n, (i - 1)));
 }
 
 /**
@@ -31,6 +31,8 @@ int _prime(int n, int i)
 int is_prime_number(int n)
 {
 	if (n <= 1)
+	{
 		return (0);
+	}
 	return (_prime(n, n - 1));
 }
